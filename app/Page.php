779d576bf26f43001bc446876161533e4f9c43ec
orlando <orlando.libardi@gmail.com>
2019-08-21 09:43:29 -0300

    Revert "atualizações"
    This reverts commit b8bb67bf056a086c4e56c99e25aa76e4ac4f8e18.

---
+++ app/Page.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace OrlandoLibardi\PageCms\app;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Page extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'alias', 'content', 'status', 'meta_title', 'meta_description', 'meta_image'
+    ];
+    /**
+     * Date updated_at Accessor
+     */   
+    public function getUpdatedAtAttribute($value)
+    {
+        if($value) return \Carbon\Carbon::parse($value)->format('d/m/Y H:i:s');
+    }
+    /**
+     * Date created_at Accessor
+     */   
+    public function getCreatedAtAttribute($value)
+    {
+        if($value) return \Carbon\Carbon::parse($value)->format('d/m/Y H:i:s');
+    }
+    /**
+     * Scope Status
+     */
+    public function scopeStatus($q){
+        return $q->where('status', 1);
+    }
+    /**
+     * Scope Alias
+     */    
+    public function scopeAlias($q, $alias){
+        return $q->status()->where('alias', '=', $alias);
+    }
+}
